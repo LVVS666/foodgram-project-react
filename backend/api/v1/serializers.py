bcff723b6156a345bd6c297b9217d615c329ea73
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-03-19 21:44:54 -0700

    send_api_foodgram

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -5,6 +5,7 @@ from djoser.serializers import UserSerializer as BaseUserSerializer
 from rest_framework.fields import ReadOnlyField
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework.validators import UniqueTogetherValidator, ValidationError
+
 from users.models import Follow
 
 from .fields import Base64ImageField
